57d5042366c70f0e069f14ac8bd66ae7e04ef87c
Steven Peng <pentwo@gmail.com>
2016-11-12 16:44:38 +0800

    module2 solved

---
+++ module2-solution/js/app.js
@@ -0,0 +1,58 @@
+(function (){
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller ('ToBuyController', ToBuyController)
+.controller ('AlreadyBoughtController', AlreadyBoughtController)
+.service ('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController (ShoppingListCheckOffService){
+	var itemToBuy = this;
+	// get item list
+	itemToBuy.items = ShoppingListCheckOffService.getItems();
+
+	// call service function to throw item to bought item list
+	itemToBuy.bought = function (itemIndex) {
+		ShoppingListCheckOffService.actionBought(itemIndex);
+	};
+
+};
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController (ShoppingListCheckOffService){
+	var itemBought = this;
+
+	// get bought item list
+	itemBought.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+};
+
+
+
+function ShoppingListCheckOffService () { 
+	var service = this;
+
+	// initial item list
+	var items = ["10 cookies","10 chips","5 soft drinks","1 instant coffee"];
+
+	// initial bought item list
+	var boughtItems = [];
+
+	// pass item list to controller
+  	service.getItems = function () {
+    	return items;
+  	};
+
+  	// pass bought item list to controller
+  	service.getBoughtItems = function () {
+  		return boughtItems;
+  	};
+
+  	// function to throw item to bought item list
+	service.actionBought = function (itemIndex) {
+		boughtItems.push(items[itemIndex]);
+		items.splice(itemIndex, 1);
+	};
+};
+
+})();
